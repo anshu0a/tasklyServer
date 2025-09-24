277f7345e32d9a062d08ca048f8f852b62676004
anshu0a <anshukumargupta000@gmail.com>
2025-09-25 03:32:31 +0530

    fixing 1001

--- config/passport.js
+++ config/passport.js
@@ -32,7 +32,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.FRONT_END}/api/auth/google/callback`
+        callbackURL: `${process.env.BACK_END}/api/auth/google/callback`
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
